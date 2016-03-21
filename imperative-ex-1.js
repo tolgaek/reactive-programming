cf8939af807c8b1f19eaad8a16109050ca8bd673
Tolga Ekmen <tolga.ekmen@gmail.com>
2016-03-21 08:29:45 -0400

    Updated examples

---
+++ imperative-ex-1.js
@@ -0,0 +1,9 @@
+// Imperative Programming
+var x = 5;
+var y = x + 1;
+
+Assert(y, 6);
+
+// Some action that changes x to 15
+x = 15;
+Asert(y, 6); // y is still 6
--- reactive-ex-1.js
+++ reactive-ex-1.js
@@ -1,16 +1,3 @@
-// Imperative Programming
-var x = 5;
-var y = x + 1;
-
-Assert(y, 6);
-
-// Some action that changes x to 15
-x = 15;
-Asert(y, 6); // y is still 6
-
-
-
-
 // Reactive Programming
 var x = 5;
 var y = x + 1;
