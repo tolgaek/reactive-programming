6bff4c856b5c25b49774b153576314f62e247fbb
Tolga Ekmen <tolga.ekmen@gmail.com>
2016-03-21 09:24:43 -0400

    Update example

--- react1.js
+++ react1.js
@@ -1,9 +1,15 @@
 React.createCreate({
+  getDefaultState: function() {
+    return {
+      name: 'Tolga'
+    }
+  },
+
   render: function() {
     return (
       <div>
         <h1>User Information</h1>
-        <UserWidget name=this.props.name />
+        <UserWidget name=this.state.name />
       </div>
     );
   }
